b0cb583bb149249f3a0d4bbd4110b1336afe5870
Nicolas Voigt <144836055+Mefanick@users.noreply.github.com>
2024-05-28 09:21:16 -0300

    Tarefa 4 concluída

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+const rootModal = document.querySelector(".root__modal");
+const modalContainer = document.querySelector("#modalContainer");
+const buttonTop = document.querySelector(".register__button");
+const buttonBot = document.querySelector(".FAQ__button--register")
+const closeButton = document.querySelector(".modal__button--close");
+
+
+buttonTop.addEventListener("click", () => {
+    modalContainer.showModal();
+});
+buttonBot.addEventListener("click", () => {
+    modalContainer.showModal();
+});
+closeButton.addEventListener("click", () => {modalContainer.close()});
+
